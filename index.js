de893884b71e63fcb23a8ce6c3ef740eebb9cbdc
rpcieszaidin <114917189+rpcieszaidin@users.noreply.github.com>
2022-11-23 10:01:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var WebSocketServer = require("ws").Server;
 
 // Create a new WebSocketServer running on port 7007.
 var wss = new WebSocketServer({port: 8023});
-
+var clientMaster = null;
+var first = true;
 // Output a log to say the server is running.
 console.log("Server is Running...");
 
@@ -11,20 +12,31 @@ console.log("Server is Running...");
 // The function will take a "msg" paramter. When called, it will
 // loop through all the connected clients and send them the msg.
 wss.broadcast = function broadcastMsg(msg) {
-    wss.clients.forEach(function each(client) {
-	const message = {
-		type: 'mensaje',
-		valor: JSON.parse(msg).mensaje
-	}
-        client.send(JSON.stringify(message));
-    });
+    let data = JSON.parse(msg);
+    const message = {
+	type: 'mensaje',
+	valor: JSON.parse(msg).mensaje
+    }
+    console.log("Desde mensajes: "+ data.tipo);
+    if (data.tipo != null) {
+    	if (data.tipo == 0) {
+    		clientMaster.send(JSON.stringify(message));
+    	} else {
+    		wss.clients.forEach(function each(client) {
+			client.send(JSON.stringify(message));
+		});
+    	}
+    }
 };
 
 // Create a listener function for the "connection" event.
 // Each time we get a connection, the following function
 // is called.
 wss.on('connection', function connection(ws) {
-
+    if (first) {
+    	clientMaster = ws;
+    	first = false;
+    }	
     // Store the remote systems IP address as "remoteIp".
     //var remoteIp = ws.upgradeReq.connection.remoteAddress;
 
